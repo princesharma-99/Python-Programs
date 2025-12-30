7f911bc5d008fd459380f42016f2105b8313ffea
Prince Sharma <bs135585@gmail.com>
2025-12-30 20:56:42 +0530

    data types

---
+++ Basics/data_types.py
@@ -0,0 +1,11 @@
+a=10
+print("a =",a,", type =",type(a))
+
+b=3.14
+print("b =",b,", type =",type(b))
+
+c="Hello, World!"
+print("c =",c,", type =",type(c))
+
+d=True
+print("d =",d,", type =",type(d))
